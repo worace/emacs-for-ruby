0a49b05a55f0a4a7334d86464fad8fe8466b1d5c
Horace Williams <horace.d.williams@gmail.com>
2016-06-06 14:29:35 -0600

    first package installation -- better defaults

---
+++ init.el
@@ -0,0 +1,21 @@
+; list the repositories containing them
+(setq package-archives '(("elpa" . "http://tromey.com/elpa/")
+                         ("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+
+; activate all the packages (in particular autoloads)
+(package-initialize)
+
+; fetch the list of packages available
+(unless package-archive-contents
+  (package-refresh-contents))
+
+; list the packages you want
+(setq package-list '(better-defaults))
+
+; install the missing packages
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
+
+(require 'better-defaults)
